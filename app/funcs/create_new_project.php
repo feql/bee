4c45966d9f686e59796dd5283dc869dcdca55e16
Nyola Mike <nyolamike@live.com>
2023-08-13 19:30:28 +0300

    docker comand through pipes

--- app/funcs/create_new_project.php
+++ app/funcs/create_new_project.php
@@ -91,8 +91,10 @@ function create_new_project(){
 
     // //start docker containers for the project
     // $compose_up_cmd = 'echo "docker ps -a" > /bee_realease_configs/feqpipe';
-    $compose_up_cmd = "echo \"docker-compose up -d -f $app_docker_compose_file_path\" > /bee_realease_configs/feqpipe";
-    var_dump($compose_up_cmd);
+    $cmd_to_send = "docker-compose up -d -f $app_docker_compose_file_path";
+    var_dump("cmd_to_send", $cmd_to_send);
+    $compose_up_cmd = 'echo "'.$cmd_to_send.'" > /bee_realease_configs/feqpipe';
+    var_dump("compose_up_cmd", $compose_up_cmd);
     shell_exec($compose_up_cmd);
 
 
